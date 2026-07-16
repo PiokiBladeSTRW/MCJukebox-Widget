0889abaaec592aa5556182e568b803c71cad8475
Pioki <151848972+PiokiBladeSTRW@users.noreply.github.com>
2026-07-16 18:41:49 +0530

    gitignore

--- build/CMakeFiles/mpdlisten_copy_qml.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  ".qt/mpdlisten_qml.txt"
-  "CMakeFiles/mpdlisten_copy_qml"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/mpdlisten_copy_qml.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
